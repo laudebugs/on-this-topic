d63498aaaffe670184e6dce653b58dc17e777622
lbugasu <lbugasu@gmail.com>
2020-11-17 00:07:18 -0500

    Some Change

--- onthistopic-frontEnd/src/pages/HomePage.js
+++ onthistopic-frontEnd/src/pages/HomePage.js
@@ -52,7 +52,7 @@ export default function HomePage() {
       <Header />
       <MenuBar />
       {displayPage()}
-      <PodCarousel podcasts />
+      <PodCarousel />
       {/* <Player /> */}
     </div>
   );
